4aa20f62be0f01e890209062956c8b710bbb6b86
eunice-nxn <medofch@gmail.com>
2021-08-12 18:32:39 +0900

    Change the type of thread_id from pthread_t to long int for transmitting runtime information through named pipe

--- ddchck.c
+++ ddchck.c
@@ -10,7 +10,7 @@
 #include <pthread.h>
 
 typedef struct Node {
-	pthread_t thread_id;
+	long int thread_id;
 	pthread_mutex_t * mutex;
 	struct Node * next;
 } Node;
@@ -19,6 +19,7 @@ typedef struct Edge {
 	struct Node * u;
 	struct Node * v;
 	struct Edge * next;
+	int visited ;
 } Edge;
 
 typedef struct Graph {
@@ -28,7 +29,7 @@ typedef struct Graph {
 	struct Edge * e_list_first; // all edges
 } Graph;
 
-Node * node_init(pthread_t thread_id, pthread_mutex_t * mutex) {
+Node * node_init(long int thread_id, pthread_mutex_t * mutex) {
 
 	Node * new = (Node * ) malloc (sizeof(Node));
 	new->thread_id = thread_id;
@@ -44,6 +45,7 @@ Edge * edge_init(Node * u, Node * v){
 	new->v = node_init(v->thread_id, v->mutex);
 	new->v->next = v->next;
 	new->next = NULL;
+	new->visited = 0;
 	return new;
 }
 
@@ -76,16 +78,14 @@ int print_graph (Graph * g){
 int delete_edge (Graph * g, Edge * e){
 
 	if(g->e_list_first == NULL){
-		perror("Node don't exist");
+		perror("Node doesn't exist");
 		exit(1);
 	} 
 
 	Edge * i = g->e_list_first;
 	Edge * prev_edge = NULL;
 
-	long int i_v_thread_id = (long int) i->v->thread_id;
-	long int e_v_thread_id = (long int) e->v->thread_id;
-	if( i_v_thread_id == e_v_thread_id && i->v->mutex == e->v->mutex)
+	if( i->v->thread_id == e->v->thread_id && i->v->mutex == e->v->mutex)
 	{	
 		g->e_list_size--;
 		g->e_list_first = i->next;
@@ -94,7 +94,7 @@ int delete_edge (Graph * g, Edge * e){
 	}
 
 	for( ; i != NULL ; i = i->next ){
-		if( i_v_thread_id == e_v_thread_id && i->v->mutex == e->v->mutex)
+		if( i->v->thread_id == e->v->thread_id && i->v->mutex == e->v->mutex)
 		{
 			break;
 		} 
@@ -109,19 +109,18 @@ int delete_edge (Graph * g, Edge * e){
 	return 0;
 }
 
+
 int delete_node (Graph * g, Node * n){
 
 	if(g->n_list_first == NULL){
-		perror("Node don't exist");
+		perror("Node doesn't exist");
 		exit(1);
 	} 
 
 	Node * i = g->n_list_first;
 	Node * prev_node = NULL;
 
-	long int i_thread_id = (long int) i->thread_id;
-	long int n_thread_id = (long int) n->thread_id;
-	if( i_thread_id == n_thread_id && i->mutex == n->mutex ){
+	if( i->thread_id == n->thread_id && i->mutex == n->mutex ){
 		g->n_list_size--;
 		g->n_list_first = i->next;
 		free(i);
@@ -129,7 +128,7 @@ int delete_node (Graph * g, Node * n){
 	}
 
 	for( ; i != NULL ; i = i->next ){
-		if( i_thread_id == n_thread_id && i->mutex == n->mutex ){
+		if( i->thread_id == n->thread_id && i->mutex == n->mutex ){
 			break;
 		} 
 		prev_node = i;
@@ -169,7 +168,7 @@ int insert_node (Graph * g, Node * n){
 	return 0;	
 }
 
-int release_lock(Graph * g, pthread_t thread_id, pthread_mutex_t * mutex){
+int release_lock(Graph * g, long int thread_id, pthread_mutex_t * mutex){
 
 	int r = -1;
 	int cnt = 0;
@@ -178,12 +177,11 @@ int release_lock(Graph * g, pthread_t thread_id, pthread_mutex_t * mutex){
 		if(j->v->mutex == mutex ) {
 			cnt++;
 			int ret = delete_edge(g, j);
-			printf("ret %d in release_lock\n", ret);
 		}
 	}
 	Node * i = g->n_list_first;
 	for( ; i != NULL ; i = i->next ){
-		if( i->mutex == mutex && pthread_equal(i->thread_id, thread_id) ){
+		if( i->mutex == mutex && i->thread_id == thread_id ){
 			r = delete_node(g, i);
 			return r;
 		}
@@ -191,16 +189,13 @@ int release_lock(Graph * g, pthread_t thread_id, pthread_mutex_t * mutex){
 	return r;
 }
 
-int acquire_lock (Graph * g, pthread_t thread_id, pthread_mutex_t * mutex){
+int acquire_lock (Graph * g, long int thread_id, pthread_mutex_t * mutex){
 
 	Node * new = node_init(thread_id, mutex);
 	int ret = insert_node (g, new);
 	Node * i = g->n_list_first;
 	for( ; i != NULL ; i = i->next){
-		long int i_thread_id = (long int) i->thread_id;
-		long int a_thread_id = (long int) thread_id;
-		if( i_thread_id == a_thread_id ){
-			printf("i->thread_id %ld thread_id %ld | pthread_equal(i->thread_id, thread_id) %d\n", i->thread_id, thread_id, pthread_equal(i->thread_id, thread_id));
+		if( i->thread_id == thread_id ){
 			Edge * new_edge = edge_init(i, new);
 			int ret = insert_edge(g, new_edge);
 		}
@@ -246,8 +241,8 @@ int main(){
 				exit(1);
 		}
 
-		pthread_t thread_id;
-		if( (r = read_bytes(fd, &thread_id, sizeof(pthread_t))) != sizeof(thread_id) ){
+		long int thread_id;
+		if( (r = read_bytes(fd, &thread_id, sizeof(long int))) != sizeof(thread_id) ){
 				perror("read bytes error\n");
 				exit(1);
 		}
